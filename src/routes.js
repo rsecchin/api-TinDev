744c644f1af4594be22237b4afa9a1126b8ff473
rSecchin <rick_gava@hotmail.com>
2019-08-07 17:34:08 -0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const routes = express.Router();
+
+const DevController = require('./controllers/DevController');
+
+
+routes.post('/devs', DevController.store);
+
+
+module.exports = routes;
